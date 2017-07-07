8a341187a3424e864911938198a636ff3843ccca
Egorchatov <aveultramar40000@mail.ru>
2017-07-07 09:17:28 -0700

    Commit to test

--- PhidgetWPF/PhidgetWPF/MainWindow.xaml.cs
+++ PhidgetWPF/PhidgetWPF/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace PhidgetWPF
         public MainWindow()
         {
             InitializeComponent();
-            //DataContext = new MainWindowViewModel();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
